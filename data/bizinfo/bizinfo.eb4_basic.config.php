90dfe650a31c8916f369f1fa85de81cc39983572
temiloluwao <temiloluwao@phillipsoutsourcing.net>
2024-11-26 16:50:04 +0100

    SecondCommit

---
+++ data/bizinfo/bizinfo.eb4_basic.config.php
@@ -0,0 +1,27 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$bizinfo = array(
+	"bi_company_name" => "주식회사 OOOO",
+	"bi_company_bizno" => "OOO-OO-OOOOO",
+	"bi_company_ceo" => "홍길동",
+	"bi_company_tel" => "000-0000-0000",
+	"bi_company_fax" => "000-0000-0000",
+	"bi_company_sellno" => "제2024-서울OO-0000호",
+	"bi_company_bugano" => "",
+	"bi_company_infoman" => "홍길동",
+	"bi_company_infomail" => "webmaster@domain.com",
+	"bi_cs_tel1" => "012-3456-7890",
+	"bi_cs_tel2" => "",
+	"bi_cs_fax" => "000-0000-0000",
+	"bi_cs_email" => "webmaster@domain.com",
+	"bi_cs_time" => "평일 09:00 ~ 18:00
+토요일 09:00 ~ 12:00",
+	"bi_cs_closed" => "일요일 및 공휴일은 휴무",
+	"bi_company_zip" => "",
+	"bi_company_addr1" => "",
+	"bi_company_addr2" => "",
+	"bi_company_addr3" => "",
+	"bi_company_addr_jibeon" => "",
+	"bi_top_logo" => "be40cd0cd01209b4a21423f85fe24611_0qN2Ezlp_cdcb9d8c559e82c7c53a93424f87f446a4765414.png",
+	"bi_top_mobile_logo" => "be40cd0cd01209b4a21423f85fe24611_kZyMS4CU_31f9d69c0e2c3f580be66c9abfc5ea7e5336cbdf.png",
+);
---
+++ data/dbconfig-1.php
@@ -0,0 +1,5 @@
+<?php
+define('G5_MYSQL_HOST','localhost');
+define('G5_MYSQL_USER','root');
+define('G5_MYSQL_PASSWORD','BiL@18');
+define('G5_MYSQL_DB','eyoom');
